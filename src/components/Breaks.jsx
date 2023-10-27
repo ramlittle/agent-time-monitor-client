b847cb264175b4b1cda36eb3ff6f5460d0a35cd7
ramelitomartinez <ramelitomartinez@gmail.com>
2023-10-27 13:52:11 -0400

    feat: now able to add breaks, but not complete conditions yet

--- src/components/Breaks.jsx
+++ src/components/Breaks.jsx
@@ -1,11 +1,78 @@
 import Header from './Header';
 import Footer from './Footer';
-const Breaks=()=>{
+import { useState } from 'react';
+import axios from 'axios';
+const Breaks = () => {
+    const userId = localStorage.getItem('userId');
+    const currentDate = new Date();
+    const dateTime = currentDate.toISOString();
+    const [showStart, setShowStart] = useState(true);
+    const [showEnd, setShowEnd] = useState(false);
+
+    console.log(dateTime);
+
+    function handleStartBreak() {
+        console.log('here sample Start Break:');
+        console.log(userId);
+        console.log(dateTime);
+        console.log('Start');
+        console.log('Break Number to be determined');
+        setShowStart(false);
+        setShowEnd(true);
+    }
+    function handleEndBreak() {
+        console.log('here sample End Break:');
+        console.log(userId);
+        console.log(dateTime);
+        console.log('End');
+        console.log('First');
+        setShowStart(true);
+        setShowEnd(false);
+
+        const configuration = {
+            method: 'post',
+            url: 'http://localhost:8080/api/v1/breaks/addbreak',
+            data: {
+                userId:userId,
+                dateTime:dateTime,
+                type:'End',
+                breakType: 'First',
+            },
+        };
+
+        // make the API call
+        axios(configuration)
+            .then((result) => {
+                alert(result.data.status);
+                // window.location.reload(false);
+            })
+            .catch((error) => {
+                alert(error.response.data.status);
+            });
+    }
+
+
     return (
         <>
-         <Header/>
-            BREAKS
-            <Footer/>
+            <Header />
+            <section>
+                <div>
+                    <span>Take Break</span>
+                    {
+                        showStart &&
+                        <button onClick={handleStartBreak}>Start</button>
+                    }
+
+                    {
+                        showEnd &&
+                        <button onClick={handleEndBreak}>End</button>
+                    }
+
+                </div>
+
+
+            </section>
+            <Footer />
         </>
     )
 }
